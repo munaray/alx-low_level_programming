e7ff66855d635c1eb96e0ec296782794f25638b1
munaray <munahdesigns@gmail.com>
2023-02-17 04:47:35 +0100

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that output a line of code with put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
