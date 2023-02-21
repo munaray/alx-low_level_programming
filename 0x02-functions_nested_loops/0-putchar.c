ad32b4fb62f319998412b41e20d4b569218698c7
munaray <munahdesigns@gmail.com>
2023-02-21 20:27:11 +0100

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-	/**
-	 * main - A program that print _putchar
-	 * Return: 0 (Success)
-	 */
+/**
+* main - A program that print _putchar
+* Return: 0 (Success)
+*/
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
-
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
