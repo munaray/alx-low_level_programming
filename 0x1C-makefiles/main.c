4bb02383fff788a961e685ffe4a34f397fb1f01b
munaray <munahdesigns@gmail.com>
2023-08-26 07:11:14 +0100

    update

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
